6cfe165ebb4b71f69aae0ef874bf73ef04e8814c
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2024-03-11 17:23:32 +0500

    setting up a project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,11 +37,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(Deps.core)
+    implementation(platform(Deps.kotlin_bom))
+    implementation(Deps.appCompat)
+    implementation(Deps.androidMaterial)
+    implementation(Deps.constraintLayout)
+    testImplementation(TestImplementation.junit)
+    androidTestImplementation(AndroidTestImplementation.junit)
+    androidTestImplementation(AndroidTestImplementation.espresso)
 }
