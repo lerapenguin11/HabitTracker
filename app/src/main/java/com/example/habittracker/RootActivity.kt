bd62b38e29693b09b6ad49bc06b3e024c61ee360
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2024-04-03 20:19:28 +0500

    refactoring RootActivity

--- app/src/main/java/com/example/habittracker/RootActivity.kt
+++ app/src/main/java/com/example/habittracker/RootActivity.kt
@@ -1,13 +1,8 @@
 package com.example.habittracker
 
-import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.ActivityResultLauncher
-import com.example.habittracker.presentation.adapter.HabitsAdapter
 import com.example.habittracker.databinding.ActivityRootBinding
-import com.example.habittracker.presentation.model.Habit
 
 class RootActivity : AppCompatActivity() {
 
