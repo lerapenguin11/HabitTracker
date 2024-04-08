754db53b46ed83f6ef1d43c3060125e0d4484d63
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2024-04-09 00:45:42 +0500

    add HabitRepositoryImpl

---
+++ app/src/main/java/com/example/habittracker/data/HabitRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package com.example.habittracker.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.habittracker.domain.repository.HabitsRepository
+import com.example.habittracker.presentation.model.Habit
+
+class HabitRepositoryImpl : HabitsRepository {
+    private val habitListLD = MutableLiveData<List<Habit>>()
+    private val habitList = mutableListOf<Habit>()
+
+    override fun getHabits(): LiveData<List<Habit>> {
+        return habitListLD
+    }
+
+    override fun getHabitItem(habitId: Int): Habit {
+        return habitList.find {
+            it.id == habitId
+        }!!
+    }
+
+    override fun updateHabit(habit: Habit) {
+        val habitIndex = habitList.indexOfFirst { it.id == habit.id }
+        if (habitIndex != -1) {
+            habitList[habitIndex] = habit
+        }
+    }
+
+    override fun createHabit(habit: Habit) {
+        habitList.add(habit)
+        updateList()
+    }
+
+    private fun updateList(){
+        habitListLD.value = habitList.toList()
+    }
+}
--- app/src/main/java/com/example/habittracker/domain/repository/HabitsRepository.kt
+++ app/src/main/java/com/example/habittracker/domain/repository/HabitsRepository.kt
@@ -7,6 +7,7 @@ import com.example.habittracker.presentation.model.Habit
 interface HabitsRepository
 {
     fun getHabits() : LiveData<List<Habit>>
+    fun getHabitItem(habitId : Int) : Habit
 
     //TODO habit_processing_domain
     fun updateHabit(habit : Habit)
