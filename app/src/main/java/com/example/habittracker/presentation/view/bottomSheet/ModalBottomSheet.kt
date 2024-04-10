e5f14ccaea08948edd04b797039f4835275dedfe
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2024-04-10 23:31:33 +0500

    the bottom sheet has been removed

---
+++ app/src/main/java/com/example/habittracker/presentation/view/bottomSheet/ModalBottomSheet.kt
@@ -0,0 +1,26 @@
+package com.example.habittracker.presentation.view.bottomSheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.habittracker.databinding.LayoutModalBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class ModalBottomSheet : BottomSheetDialogFragment()
+{
+    private var _binding : LayoutModalBottomSheetBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        _binding = LayoutModalBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        const val TAG = "ModalBottomSheet"
+    }
+}
