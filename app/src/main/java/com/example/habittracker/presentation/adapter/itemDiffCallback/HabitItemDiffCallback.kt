e692b6965ca5a35e03eaf436db29a740b56c098f
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2024-04-24 20:37:22 +0500

    fix diffCallback

--- app/src/main/java/com/example/habittracker/presentation/adapter/itemDiffCallback/HabitItemDiffCallback.kt
+++ app/src/main/java/com/example/habittracker/presentation/adapter/itemDiffCallback/HabitItemDiffCallback.kt
@@ -6,8 +6,7 @@ import com.example.habittracker.domain.model.Habit
 class HabitItemDiffCallback : DiffUtil.ItemCallback<Habit>()
 {
     override fun areItemsTheSame(oldItem: Habit, newItem: Habit): Boolean {
-        //return oldItem.id == newItem.id
-        return oldItem == newItem
+        return oldItem.id == newItem.id
     }
 
     override fun areContentsTheSame(oldItem: Habit, newItem: Habit): Boolean {
